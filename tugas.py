016ad9ef56958876a6cddc330b1c764f1b879c56
k-jank <157026427+k-jank@users.noreply.github.com>
2024-05-06 10:38:27 +0700

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,31 +1,31 @@
 import streamlit as st
 import pandas as pd
-from catboost import CatBoostClassifier
 import altair as alt
 import numpy as np
 import matplotlib.pyplot as plt
+from catboost import CatBoostClassifier
 
 # Load the trained model
 model = CatBoostClassifier()
 model.load_model('catboost_model.bin')
 
-# Uploading Excel file
+# Widget untuk mengunggah file Excel
 uploaded_file = st.sidebar.file_uploader("Upload Excel file to analyze players", type=["xlsx", "xls"])
 
 if uploaded_file is None:
     xls = pd.ExcelFile('feature.xlsx')
     sheet_names_upper = [sheet.upper() for sheet in xls.sheet_names]
     sheet_names_upper.sort() 
-    selected_sheet = st.sidebar.selectbox("Important Attributes for Role:", [sheet.upper() for sheet in sheet_names_upper])
+    selected_sheet = st.sidebar.selectbox("Feature Importance for", [sheet.upper() for sheet in sheet_names_upper])
     selected_sheet_lower = xls.sheet_names[sheet_names_upper.index(selected_sheet)].lower()
     feature_importance_data = pd.read_excel('feature.xlsx', sheet_name=selected_sheet_lower)
     feature_importance_data_sorted = feature_importance_data.sort_values(by='feature_importance', ascending=False)
 
-    # Chart
-    st.markdown(f"<h2 style='text-align: center;'>Important Attributes for {selected_sheet}</h2>", unsafe_allow_html=True)
+    # Set title for the chart
+    st.markdown(f"<h2 style='text-align: center;'>Feature Importance for {selected_sheet}</h2>", unsafe_allow_html=True)
     chart = alt.Chart(feature_importance_data_sorted).mark_bar().encode(
-        y=alt.Y('Attributes', sort='-x'),  # Sorting by feature importance descending
-        x='Value'
+        y=alt.Y('feature_name', sort='-x'),  # Sorting by feature importance descending
+        x='feature_importance'
     ).properties(
         width=600,
         height=400
@@ -33,29 +33,34 @@ if uploaded_file is None:
 
     st.altair_chart(chart, use_container_width=True)
 
+# Jika file telah diunggah
 if uploaded_file is not None:
-    # Reading data
+    # Membaca data dari file Excel
     data = pd.read_excel(uploaded_file)
     data = data[model.feature_names_]
 
-    # Make predictions using the model
+        # Make predictions using the model
     predictions = model.predict(data)
+
+        # Convert the nested list to a flat list
     predictions_flat = predictions.flatten()
 
-    # Data
+        # Data
     fm = pd.read_excel(uploaded_file)
     hasil_prediksi_df = pd.DataFrame(fm)
     hasil_prediksi_df['Best Role'] = predictions_flat
 
-    # Display dropdown
+        # Tampilkan dropdown untuk memilih posisi
     selected_position = st.sidebar.selectbox("Choose Player Role:", sorted(hasil_prediksi_df['Best Role'].unique()))
+
+        # Filter DataFrame based on selected position
     filtered_df = hasil_prediksi_df[hasil_prediksi_df['Best Role'] == selected_position]
     unique_name = sorted(filtered_df['Name'].unique())
     selected_players = st.sidebar.multiselect("Players:", unique_name)
 
-    # Handle comparison between selected players
+        # Handle comparison between selected players
     if selected_players:
-        # Display player information for all selected players
+            # Display player information for all selected players
         st.markdown(f"<h1 style='text-align: center;'>Football Manager Player Analyzer</h1>", unsafe_allow_html=True)
         num_players = len(selected_players)
         cols = st.columns(num_players)
@@ -71,7 +76,7 @@ if uploaded_file is not None:
                 st.markdown(f"<div style='text-align: center; padding: 10px; margin-bottom: 10px; border-radius: 5px; background-color: {colors[i % len(colors)]}; color: white;'><b>CA</b><br>{selected_data['CA'].iloc[0]}</div>", unsafe_allow_html=True)
                 st.markdown(f"<div style='text-align: center; padding: 10px; margin-bottom: 10px; border-radius: 5px; background-color: {colors[i % len(colors)]}; color: white;'><b>PA</b><br>{selected_data['PA'].iloc[0]}</div>", unsafe_allow_html=True)
 
-        # Generate radar plot
+        # Generate radar plot for all selected players
         fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
         num_cols = len(filtered_df.columns) - 7 
         angles = np.linspace(0, 2 * np.pi, num_cols, endpoint=False).tolist()
