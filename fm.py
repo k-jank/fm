6e93cf83db6fd518a3e5e830209a504a05c51e66
k-jank <157026427+k-jank@users.noreply.github.com>
2024-05-10 13:58:02 +0700

    Update fm.py

--- fm.py
+++ fm.py
@@ -3,7 +3,7 @@ import pandas as pd
 import altair as alt
 import numpy as np
 import matplotlib.pyplot as plt
-imoport os
+import os
 from catboost import CatBoostClassifier
 
 # Load the trained model
